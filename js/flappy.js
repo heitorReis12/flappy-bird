662e5f43d10e1a0255f585f9c348c058ee24175d
Heitor <heitorredigolo9@gmail.com>
2025-01-30 11:46:10 -0300

    contando pontos

--- js/flappy.js
+++ js/flappy.js
@@ -57,9 +57,9 @@ function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
         par.sortearAbertura();
       }
       const meio = largura / 2;
-      const cruzouOMeio =
-        par.getX() + deslocamento >= meio && par.getX() < meio;
-      if (cruzouOMeio) notificarPonto;
+      const cruzouOMeio = par.getX() + deslocamento >= meio
+                && par.getX() < meio
+            if(cruzouOMeio) notificarPonto()
     });
   };
 }
@@ -90,12 +90,46 @@ function Passaro(alturajogo) {
   this.setY(alturajogo / 2);
 }
 
+
+function Progresso(){
+  this.elemento = novoElemento('span', 'progresso')
+  this.atualizarPontos = pontos => {
+    this.elemento.innerHTML = pontos
+  }
+  this.atualizarPontos(0)
+}
 // const barreiras = new Barreiras(700, 1200, 300, 400);
 // const passaro = new Passaro(700);
 // const areaDoJogo = document.querySelector("[wm-flappy]");
 // areaDoJogo.appendChild(passaro.elemento);
+// areaDoJogo.appendChild(new Progresso().elemento)
 // barreiras.pares.forEach((par) => areaDoJogo.appendChild(par.elemento));
 // setInterval(() => {
 //   barreiras.animar();
 //   passaro.animar();
 // }, 20);
+
+function FlappyBird(){
+  let pontos = 0;
+  const areaDoJogo = document.querySelector('[wm-flappy]')
+  const altura = areaDoJogo.clientHeight
+  const largura = areaDoJogo.clientWidth
+
+  const progresso = new Progresso()
+  const barreiras = new Barreiras(altura, largura, 300, 400, ()=>progresso.atualizarPontos(++pontos))
+  const passaro = new Passaro(altura)
+
+  areaDoJogo.appendChild(progresso.elemento)
+  areaDoJogo.appendChild(passaro.elemento)
+  barreiras.pares.forEach(par=> areaDoJogo.appendChild(par.elemento))
+
+  this.start = () => {
+    //loop do jogo
+    const temporizador = setInterval(()=>{
+      barreiras.animar()
+      passaro.animar()
+    },20)
+  }
+}
+
+new FlappyBird().start()
